9d31a7399fc075beb3979bef6be5d235af866971
Rodion Ventslovsky <vents_23@mail.ru>
2025-08-05 01:29:41 +0300

    add update func mask_account_card

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,7 @@ def get_mask_card_number(card_number: str) -> str:
     return mask_card
 
 
-def get_mask_account(account_number: str) -> str:
+def get_mask_check(check_number: str) -> str:
     """Функцию маскировки номера банковского счета"""
-    mask_account = f"**{''.join(account_number[-4:])}"
-    return mask_account
+    mask_check = f"**{''.join(check_number[-4:])}"
+    return mask_check
