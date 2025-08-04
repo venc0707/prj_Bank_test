16030cd6dcfa8e10b255ca387655e94de55999d7
Rodion Ventslovsky <vents_23@mail.ru>
2025-08-05 01:27:37 +0300

    add func mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,33 @@
+from masks import get_mask_card_number, get_mask_check
+import re
+
+
+def mask_account_card(type_number: str) -> list:
+    """ Функция возвращает: тип карты и номер(счет) карты """
+    split_type_number = type_number.split()
+    type_card = []
+    ac_number = ''
+    mask_card = ''
+    for item in split_type_number:
+        if re.fullmatch(r"^[a-zA-Z]+$", item):
+            type_card.append(item)
+            type_card_join = ' '.join(type_card)
+        elif re.fullmatch(r"^[а-яёА-ЯЁ]+$", item):
+            type_card.append(item)
+            type_card_join = ' '.join(type_card)
+        if item.isdigit():
+            ac_number += item
+    if len(ac_number) == 20:
+        mask_card = get_mask_check(ac_number)
+    elif len(ac_number) == 16:
+        mask_card = get_mask_card_number(ac_number)
+
+    return f'{type_card_join} {mask_card}'
+
+
+
+if __name__ == '__main__':
+    #  type_number = 'Maestro 1596837868705199'
+    #type_number = 'Visa Platinum 8990922113665229'
+    type_number = 'Счет 35383033474447895560'
+    print(mask_account_card(type_number))  # Visa Platinum 7000 79** **** 6361 | Счет **4305
