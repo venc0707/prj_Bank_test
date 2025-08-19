22b14e984662a0da55a8316235840a1e9dafb13c
Rodion Ventslovsky <vents_23@mail.ru>
2025-08-19 20:48:23 +0300

    add func_card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -1,5 +1,9 @@
+from types import new_class
 
-def filter_by_currency(transactions: list[dict], currency_name: str):
+from pygments.styles.dracula import yellow
+
+
+def filter_by_currency(transactions: list[dict], currency_name: str) -> dict:
     """возвращает итератор с транзакциями по названию валюты"""
     currency_name = currency_name.upper()
     transactions = (transaction for transaction in transactions if transaction['operationAmount']['currency']['code'] == currency_name )
@@ -13,6 +17,18 @@ def transaction_descriptions(transactions: list[dict]) -> str:
     for description in descriptions:
         yield description
 
+
+def card_number_generator(stars: int, end: int) -> str:
+    """выдает номера банковских карт в формате
+    XXXX XXXX XXXX XXXX"""
+    numbers = range(stars, end+1)
+    mask_card = '0000000000000000'
+
+    for num in numbers:
+        new_card = mask_card[:len(mask_card)-len(str(num))]
+        new_card += str(num)
+
+        yield (f'{new_card[:4]} {new_card[4:8]} {new_card[8:12]} {new_card[12:16]}')
 
 
 if __name__ == "__main__":
@@ -101,35 +117,6 @@ for _ in range(2):
 descriptions = transaction_descriptions(transactions)
 for _ in range(5):
     print(next(descriptions))
-#     filter_by_currency(transactions, 'USD')
-# 
-# # >>> {
-# #           "id": 939719570,
-# #           "state": "EXECUTED",
-# #           "date": "2018-06-30T02:08:58.425572",
-# #           "operationAmount": {
-# #               "amount": "9824.07",
-# #               "currency": {
-# #                   "name": "USD",
-# #                   "code": "USD"
-# #               }
-# #           },
-# #           "description": "Перевод организации",
-# #           "from": "Счет 75106830613657916952",
-# #           "to": "Счет 11776614605963066702"
-# #       }
-# #       {
-# #               "id": 142264268,
-# #               "state": "EXECUTED",
-# #               "date": "2019-04-04T23:20:05.206878",
-# #               "operationAmount": {
-# #                   "amount": "79114.93",
-# #                   "currency": {
-# #                       "name": "USD",
-# #                       "code": "USD"
-# #                   }
-# #               },
-# #               "description": "Перевод со счета на счет",
-# #               "from": "Счет 19708645243227258542",
-# #               "to": "Счет 75651667383060284188"
-#        }
+
+for card_number in card_number_generator(1, 5):
+    print(card_number)
