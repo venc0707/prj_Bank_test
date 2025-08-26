ffd24a8c008e8403ebb1450d999d99cf958baf95
Rodion Ventslovsky <vents_23@mail.ru>
2025-08-26 13:05:27 +0300

    flake8 mypy isort

--- src/generators.py
+++ src/generators.py
@@ -1,130 +1,31 @@
-from types import new_class
-
-from pygments.styles.dracula import yellow
+from typing import Generator, Dict, Any, Iterator
 
 
-def filter_by_currency(transactions: list[dict], currency_name: str) -> dict:
+def filter_by_currency(transactions: list[Dict[str, Any]], currency_name: str) -> Iterator[Dict[str, Any]]:
     """возвращает итератор с транзакциями по названию валюты"""
     currency_name = currency_name.upper()
-    transactions = (transaction for transaction in transactions if transaction['operationAmount']['currency']['code'] == currency_name )
-
     for transaction in transactions:
-        yield transaction
+        if transaction["operationAmount"]["currency"]["code"] == currency_name:
+            yield transaction
 
 
-def transaction_descriptions(transactions: list[dict]) -> str:
-    """возвращает описание каждой операции """
-    descriptions = (transaction['description'] for transaction in transactions)
+def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
+    """возвращает описание каждой операции"""
+    descriptions = (transaction["description"] for transaction in transactions)
     for description in descriptions:
         yield description
 
 
-def card_number_generator(start: int, end: int) -> str:
+def card_number_generator(start: int, end: int) -> Iterator[str]:
     """выдает номера банковских карт в формате
     XXXX XXXX XXXX XXXX"""
-    numbers = range(start, end+1)
-    mask_card = '0000000000000000'
+    numbers = range(start, end + 1)
+    mask_card = "0000000000000000"
     if start >= 1 and end <= 9999999999999999:
         for num in numbers:
-            new_card = mask_card[:len(mask_card)-len(str(num))]
+            new_card = mask_card[: len(mask_card) - len(str(num))]
             new_card += str(num)
 
-            yield (f'{new_card[:4]} {new_card[4:8]} {new_card[8:12]} {new_card[12:16]}')
+            yield (f"{new_card[:4]} {new_card[4:8]} {new_card[8:12]} {new_card[12:16]}")
     else:
-        raise ValueError('неверный диапазон')
-
-if __name__ == "__main__":
-    transactions =    [
-            {
-                "id": 939719570,
-                "state": "EXECUTED",
-                "date": "2018-06-30T02:08:58.425572",
-                "operationAmount": {
-                    "amount": "9824.07",
-                    "currency": {
-                        "name": "USD",
-                        "code": "USD"
-                    }
-                },
-                "description": "Перевод организации",
-                "from": "Счет 75106830613657916952",
-                "to": "Счет 11776614605963066702"
-            },
-            {
-                "id": 142264268,
-                "state": "EXECUTED",
-                "date": "2019-04-04T23:20:05.206878",
-                "operationAmount": {
-                    "amount": "79114.93",
-                    "currency": {
-                        "name": "USD",
-                        "code": "USD"
-                    }
-                },
-                "description": "Перевод со счета на счет",
-                "from": "Счет 19708645243227258542",
-                "to": "Счет 75651667383060284188"
-            },
-            {
-                "id": 873106923,
-                "state": "EXECUTED",
-                "date": "2019-03-23T01:09:46.296404",
-                "operationAmount": {
-                    "amount": "43318.34",
-                    "currency": {
-                        "name": "руб.",
-                        "code": "RUB"
-                    }
-                },
-                "description": "Перевод со счета на счет",
-                "from": "Счет 44812258784861134719",
-                "to": "Счет 74489636417521191160"
-            },
-            {
-                "id": 895315941,
-                "state": "EXECUTED",
-                "date": "2018-08-19T04:27:37.904916",
-                "operationAmount": {
-                    "amount": "56883.54",
-                    "currency": {
-                        "name": "USD",
-                        "code": "USD"
-                    }
-                },
-                "description": "Перевод с карты на карту",
-                "from": "Visa Classic 6831982476737658",
-                "to": "Visa Platinum 8990922113665229"
-            },
-            {
-                "id": 594226727,
-                "state": "CANCELED",
-                "date": "2018-09-12T21:27:25.241689",
-                "operationAmount": {
-                    "amount": "67314.70",
-                    "currency": {
-                        "name": "руб.",
-                        "code": "RUB"
-                    }
-                },
-                "description": "Перевод организации",
-                "from": "Visa Platinum 1246377376343588",
-                "to": "Счет 14211924144426031657"
-            }
-        ]
-
-    usd_transactions = filter_by_currency(transactions, "usd")
-    try:
-        for _ in range(3):
-            print(next(usd_transactions))
-    except StopIteration:
-        print(f"Не найдено транзакций в указанной валюте")
-
-    descriptions = transaction_descriptions(transactions)
-    try:
-        for _ in range(5):
-            print(next(descriptions))
-    except StopIteration:
-        print(f"Не найдено транзакций в указанной валюте")
-
-    for card_number in card_number_generator(1, 5):
-        print(card_number)
+        raise ValueError("неверный диапазон")
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,39 +1,47 @@
 import pytest
 
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
 def test_filter_by_currency(transactions):
-    generator = filter_by_currency(transactions, 'rub')
-    assert next(generator) == {'id': 873106923, 'state': 'EXECUTED', 'date': '2019-03-23T01:09:46.296404', 'operationAmount': {'amount': '43318.34', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 44812258784861134719', 'to': 'Счет 74489636417521191160'}
-    assert next(generator) == {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689', 'operationAmount': {'amount': '67314.70', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод организации', 'from': 'Visa Platinum 1246377376343588', 'to': 'Счет 14211924144426031657'}
+    generator = filter_by_currency(transactions, "rub")
+    assert next(generator) == {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    }
+    assert next(generator) == {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657",
+    }
 
 
 def test_filter_by_currency_list():
     """Тестирование случая, когда нет подходящих транзакций"""
-    generator = list(filter_by_currency([], 'eur'))
+    generator = list(filter_by_currency([], "eur"))
     assert generator == []
 
 
 def test_filter_by_currency_not_znach():
     """Тестирование случая, когда нет подходящих транзакций"""
-    transactions = [
-        {
-            "operationAmount": {
-                "currency": {
-                    "code": "EUR"
-                }
-            }
-        }
-    ]
-    generator = list(filter_by_currency(transactions, 'usd'))
+    transactions = [{"operationAmount": {"currency": {"code": "EUR"}}}]
+    generator = list(filter_by_currency(transactions, "usd"))
     assert generator == []
 
 
 def test_transaction_descriptions(transactions):
     generator = transaction_descriptions(transactions)
-    assert next(generator) == 'Перевод организации'
-    assert next(generator) == 'Перевод со счета на счет'
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
 
 
 def test_transaction_descriptions_not():
@@ -42,26 +50,17 @@ def test_transaction_descriptions_not():
 
 
 def test_transaction_descriptions_none():
-    transactions = [
-        {
-            "description": "",
-            "operationAmount": {
-                "currency": {
-                    "code": "EUR"
-                }
-            }
-        }
-    ]
+    transactions = [{"description": "", "operationAmount": {"currency": {"code": "EUR"}}}]
     generator = transaction_descriptions(transactions)
-    assert list(generator) == ['']
+    assert list(generator) == [""]
 
 
 def test_card_number_generator():
     generator = card_number_generator(10000, 10003)
-    assert next(generator) == '0000 0000 0001 0000'
-    assert next(generator) == '0000 0000 0001 0001'
-    assert next(generator) == '0000 0000 0001 0002'
-    assert next(generator) == '0000 0000 0001 0003'
+    assert next(generator) == "0000 0000 0001 0000"
+    assert next(generator) == "0000 0000 0001 0001"
+    assert next(generator) == "0000 0000 0001 0002"
+    assert next(generator) == "0000 0000 0001 0003"
 
 
 def test_card_number_generator_error1():
@@ -74,8 +73,3 @@ def test_card_number_generator_error2():
     generator = card_number_generator(9999999999999998, 10000000000000000)
     with pytest.raises(ValueError):
         next(generator)
-
-
-
-
-
