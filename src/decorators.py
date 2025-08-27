1e548ccd6708c7b9bfda97634c68c408fa02c4e3
Rodion Ventslovsky <vents_23@mail.ru>
2025-08-27 16:25:00 +0300

    update test_log

--- src/decorators.py
+++ src/decorators.py
@@ -1,8 +1,10 @@
 from functools import wraps
 from typing import Callable
+from os.path import dirname, join
 
 
 def log(filename=None):
+    """логирование функций"""
     def wapper(func: Callable):
         @wraps(func)
         def iter(*args, **kwargs):
@@ -28,15 +30,14 @@ def log(filename=None):
     return wapper
 
 
-# Правильное использование декоратора
-@log('my_log.txt')  # Декоратор с параметром
-def text(a):
-    if a > 0:
-        return 10 / a
-    raise ValueError('отрицательное')
-
-
-# Тестирование
-if __name__ == '__main__':
-    result = text(0)
-    print(result)
+# # Тестирование
+# if __name__ == '__main__':  #
+#     # Правильное использование декоратора
+#     @log('my_log.txt')  # Декоратор с параметром
+#     def text(a):
+#         if a > 0:
+#             return 10 / a
+#         raise ValueError('отрицательное')
+#
+#     result = text(2)
+#     print(result)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,9 +1,11 @@
 import pytest
 from src.decorators import log
+from os.path import dirname, join
 
 
 
 def test_log(capsys):
+    '''тест на успешный вызов'''
     @log()
     def text(a):
         if a > 0:
@@ -14,8 +16,16 @@ def test_log(capsys):
     captured = capsys.readouterr()
     assert captured.out == 'text: completed\n'
 
+def test_log_my_log_txt():
+    ''' проверяем записи в логах'''
+    my_log = '/home/user/PycharmProjects/Prj_Bank/src/my_log.txt'
+    with open(my_log, 'r', encoding='utf-8') as f:
+        content = f.read()
+        assert "text: completed" in content
+
 
 def test_log_zero():
+    '''тест на ошибку'''
     @log()
     def text(a):
         if a > 0:
@@ -24,11 +34,3 @@ def test_log_zero():
     
     with pytest.raises(ValueError, match='отрицательное'):
         text(0)
-
-
-
-
-    
-
-    
-
